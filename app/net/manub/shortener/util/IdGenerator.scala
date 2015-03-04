29457646ea1346bf44b610d98d876eb4d92881eb
Emanuele Blanco <emanuele.blanco@gmail.com>
2015-03-04 16:46:03 +0000

    Added some docs for IdGenerator.generateRandomId

--- app/net/manub/shortener/util/IdGenerator.scala
+++ app/net/manub/shortener/util/IdGenerator.scala
@@ -4,7 +4,11 @@ import scala.util.Random
 
 object IdGenerator {
 
-  // TODO: scaladoc
+  /**
+   * Generates a random id composed of 6 random alphanumeric characters.
+   *
+   * @return a random id composed of 6 random alphanumeric characters.
+   */
   def generateRandomId: String = Random.alphanumeric.take(6).mkString
 
 }
